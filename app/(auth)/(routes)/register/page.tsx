07d6747a79bc4670084bf083a42024ff56f9e604
AlenChernick <crazyguy771@gmail.com>
2023-09-30 17:43:20 +0300

    project setup

---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const RegisterPage: NextPage = () => {
+  return <div>RegisterPage</div>;
+};
+
+export default RegisterPage;
---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return <div>This is a protected route</div>;
+};
+
+export default Home;
