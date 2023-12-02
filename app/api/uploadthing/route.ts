64cc7e2f4182127826c2d94df638ca43d3711752
AlenChernick <crazyguy771@gmail.com>
2023-12-02 18:55:25 +0200

    fixed uploadthing in production

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -2,7 +2,15 @@ import { createNextRouteHandler } from 'uploadthing/next';
 
 import { ourFileRouter } from '@/app/api/uploadthing/core';
 
+const uploadthingAppId = process.env.UPLOADTHING_APP_ID;
+const uploadthingSecret = process.env.UPLOADTHING_SECRET;
+
 // Export routes for Next App Router
 export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
+  config: {
+    uploadthingId: uploadthingAppId,
+    uploadthingSecret: uploadthingSecret,
+    callbackUrl: 'https://discordclone-production-b545.up.railway.app/api/uploadthing',
+  },
 });
